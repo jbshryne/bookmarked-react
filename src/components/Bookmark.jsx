c1ec80a60026a699ca500abd40a42cfde496a1cf
Lia Kim <j.liakim23@gmail.com>
2023-09-23 19:38:39 -0400

    trying to update

--- src/components/Bookmark.jsx
+++ src/components/Bookmark.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from 'react';
 
-const Bookmark = ({data}) => {
-    console.log(data)
+const Bookmark = ({ data, handleDeleteBookmark, setIdToUpdate }) => {
+  const handleDelete = (e) => {
+    const id = e.target.value;
+    return handleDeleteBookmark(id);
+  };
+
+  const handleUpdate = () => {
+    // Call the setIdToUpdate function from props to set the ID for editing
+    setIdToUpdate(data._id);
+  };
+
   return (
     <div>
       {data.title}
+      <button onClick={(e) => handleDelete(e)} value={data._id}>X</button>
+      <button onClick={handleUpdate}>Edit</button>
     </div>
-  )
-}
+    
+  );
+};
 
-export default Bookmark
+export default Bookmark;
