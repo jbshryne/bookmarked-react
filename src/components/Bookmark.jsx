1f27c969e2aad0f0a336ab56398e2b95cc678582
Jon Brennan <jonbrennan@Jons-MacBook-Pro.local>
2023-09-24 09:06:46 -0400

    fixed update function, general cleanup

--- src/components/Bookmark.jsx
+++ src/components/Bookmark.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const Bookmark = ({ data, handleDeleteBookmark, setIdToUpdate }) => {
   const handleDelete = (e) => {
@@ -7,19 +7,17 @@ const Bookmark = ({ data, handleDeleteBookmark, setIdToUpdate }) => {
   };
 
   const handleUpdate = () => {
-    // Call the setIdToUpdate function from props to set the ID for editing
     setIdToUpdate(data._id);
   };
 
   return (
-    <div className='bookmark'>
-     <a href={data.url} >
-        {data.title}
-     </a>
-      <button onClick={(e) => handleDelete(e)} value={data._id}>X</button>
+    <div className="bookmark">
+      <a href={data.url}>{data.title}</a>
+      <button onClick={(e) => handleDelete(e)} value={data._id}>
+        X
+      </button>
       <button onClick={handleUpdate}>Edit</button>
     </div>
-    
   );
 };
 
